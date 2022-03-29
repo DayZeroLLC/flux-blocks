138af37991a9aeeecd7f5b56548e05d0c3a0551a
chad <chad@pop-os.localdomain>
2022-03-28 20:47:46 -0400

    init

---
+++ src/components/fluxbreadcrumb/FluxBreadcrumb.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	/** Set to `true` to hide the breadcrumb trailing slash */
+	export let noTrailingSlash = false;
+	/** Set to `true` to display skeleton state */
+	export let skeleton = false;
+	export let items: BreadcrumbItem[] = [];
+	import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+  
+<Breadcrumb noTrailingSlash={noTrailingSlash} skeleton={skeleton}>
+	{#each items as thing (thing.id)}
+	<BreadcrumbItem href={thing.href}
+	isCurrentPage={thing.isCurrentPage}
+	>{thing.title}</BreadcrumbItem>
+	{/each}	
+</Breadcrumb>
