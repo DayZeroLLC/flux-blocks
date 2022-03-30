920c5ce515627eba37cc0ff29b317dd84715ead8
chad <chad@pop-os.localdomain>
2022-03-30 08:38:02 -0400

    SK

---
+++ package/FluxBreadcrumb.wc.svelte.d.ts
@@ -0,0 +1,19 @@
+import { SvelteComponentTyped } from "svelte";
+import { BreadcrumbItem } from "carbon-components-svelte";
+declare const __propDef: {
+    props: {
+        /** Set to `true` to hide the breadcrumb trailing slash */ noTrailingSlash?: boolean;
+        /** Set to `true` to display skeleton state */ skeleton?: boolean;
+        items?: BreadcrumbItem[];
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export declare type FluxBreadcrumbProps = typeof __propDef.props;
+export declare type FluxBreadcrumbEvents = typeof __propDef.events;
+export declare type FluxBreadcrumbSlots = typeof __propDef.slots;
+export default class FluxBreadcrumb extends SvelteComponentTyped<FluxBreadcrumbProps, FluxBreadcrumbEvents, FluxBreadcrumbSlots> {
+}
+export {};
